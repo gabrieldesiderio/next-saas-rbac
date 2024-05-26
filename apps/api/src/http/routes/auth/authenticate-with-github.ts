0a3cadf551067f66a6bf4c3765caa4b86876b8a1
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-05-26 12:07:47 -0300

    feat: global environment variables and authenticated routes on openapi docs

--- apps/api/src/http/routes/auth/authenticate-with-github.ts
+++ apps/api/src/http/routes/auth/authenticate-with-github.ts
@@ -1,3 +1,4 @@
+import { env } from '@saas/env'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
@@ -28,14 +29,14 @@ export async function authenticateWithGithub(app: FastifyInstance) {
         'https://github.com/login/oauth/access_token',
       )
 
-      githubOAuthURL.searchParams.set('client_id', 'Ov23lisgBcYL2HPwRACn')
+      githubOAuthURL.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
       githubOAuthURL.searchParams.set(
         'client_secret',
-        '39ec5dfd225b546d3f504db42e1d3e29a888593a',
+        env.GITHUB_OAUTH_CLIENT_SECRET,
       )
       githubOAuthURL.searchParams.set(
         'redirect_uri',
-        'http://localhost:3000/api/auth/callback',
+        env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
       )
       githubOAuthURL.searchParams.set('code', code)
 
