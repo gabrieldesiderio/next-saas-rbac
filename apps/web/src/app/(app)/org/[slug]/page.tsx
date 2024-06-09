0156749d8bd24527fd87d1e00dc3e8b4ee50b25e
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-06-08 21:36:55 -0300

    feat: intercept create organizartion route

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -1,3 +1,12 @@
+import { Header } from '@/components/header'
+
 export default async function Projects() {
-  return <h1>Projects</h1>
+  return (
+    <div className="py-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <p>Projects</p>
+      </main>
+    </div>
+  )
 }
